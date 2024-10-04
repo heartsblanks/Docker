d5f6115666b7c4d53b4672bc3c0c11554820ae40
heartsblanks <125401145+heartsblanks@users.noreply.github.com>
2024-10-04 08:45:41 +0200

    Update command_executor.py

--- command_executor.py
+++ command_executor.py
@@ -7,11 +7,12 @@ app = Flask(__name__)
 def run_command():
     # Get the command from the POST request
     command = request.json.get('command')
-    
+
     if command:
         try:
-            # Execute the command using subprocess
-            result = subprocess.run(command, shell=True, capture_output=True, text=True)
+            # PowerShell Remoting command to run on Windows host
+            powershell_command = f'powershell.exe Invoke-Command -ComputerName localhost -ScriptBlock {{ {command} }}'
+            result = subprocess.run(powershell_command, shell=True, capture_output=True, text=True)
             return jsonify({
                 'output': result.stdout,
                 'error': result.stderr,
